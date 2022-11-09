96e8a4e7fb6a3fcb405aa9ce3f1109fc164e9705
Rémi Lapeyre <remi.lapeyre@lenstra.fr>
2022-11-09 15:55:04 +0000

    Generate documentation

--- examples/resources/cloudtemple_compute_virtual_machine/resource.tf
+++ examples/resources/cloudtemple_compute_virtual_machine/resource.tf
@@ -1,19 +1,21 @@
 data "cloudtemple_compute_virtual_datacenter" "dc" {
   name = "DC-EQX6"
-} 
+}
 
 data "cloudtemple_compute_host_cluster" "flo" {
   name = "clu002-ucs01_FLO"
-} 
+}
 
 data "cloudtemple_compute_datastore_cluster" "koukou" {
   name = "sdrs001-LIVE_KOUKOU"
-} 
+}
 
 
 resource "cloudtemple_compute_virtual_machine" "foo" {
-  name = "test-terraform"
-  power_state = "on"
+  name        = "demo-terraform"
+  power_state = "off"
+
+  template = "..."
 
   virtual_datacenter_id        = data.cloudtemple_compute_virtual_datacenter.dc.id
   host_cluster_id              = data.cloudtemple_compute_host_cluster.flo.id
