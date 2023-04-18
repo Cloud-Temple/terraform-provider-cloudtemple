6542384a427c1ff37f9060b85a7b66f442e39b59
Paul BESRET <paul.besret@cloud-temple.com>
2023-04-18 17:43:45 +0200

    Updated examples of cloudtemple_compute_virtual_machine

--- examples/resources/cloudtemple_compute_virtual_machine/resource.tf
+++ examples/resources/cloudtemple_compute_virtual_machine/resource.tf
@@ -11,8 +11,8 @@ data "cloudtemple_compute_datastore_cluster" "SDRS_001" {
 }
 
 # Deploying a new virtual machine with a given operating system
-resource "cloudtemple_compute_virtual_machine" "pbt_tf_delete_powered_on" {
-  name = "pbt_tf_delete_powered_on"
+resource "cloudtemple_compute_virtual_machine" "from_scratch" {
+  name = "from_scratch"
 
   memory                 = 8 * 1024 * 1024 * 1024
   cpu                    = 2
@@ -30,57 +30,57 @@ resource "cloudtemple_compute_virtual_machine" "pbt_tf_delete_powered_on" {
   tags = {
     created_by = "Terraform"
   }
-
+ 
   lifecycle {
     prevent_destroy = true
   }
 }
 
 # Clone an already existing virtual machine
-# resource "cloudtemple_compute_virtual_machine" "cloned" {
-#   name = "cloned"
+resource "cloudtemple_compute_virtual_machine" "cloned" {
+  name = "cloned"
 
-#   clone_virtual_machine_id = cloudtemple_compute_virtual_machine.scratch.id
+  clone_virtual_machine_id = cloudtemple_compute_virtual_machine.scratch.id
 
-#   datacenter_id        = data.cloudtemple_compute_virtual_datacenter.dc.id
-#   host_cluster_id      = data.cloudtemple_compute_host_cluster.flo.id
-#   datastore_cluster_id = data.cloudtemple_compute_datastore_cluster.koukou.id
+  datacenter_id        = data.cloudtemple_compute_virtual_datacenter.dc.id
+  host_cluster_id      = data.cloudtemple_compute_host_cluster.flo.id
+  datastore_cluster_id = data.cloudtemple_compute_datastore_cluster.koukou.id
 
-#   tags = {
-#     created_by = "Terraform"
-#   }
-# }
+  tags = {
+    created_by = "Terraform"
+  }
+}
 
 # Deploy an item from the content library
-# data "cloudtemple_compute_content_library" "public" {
-#   name = "PUBLIC"
-# }
+data "cloudtemple_compute_content_library" "public" {
+  name = "PUBLIC"
+}
 
-# data "cloudtemple_compute_content_library_item" "centos" {
-#   content_library_id = data.cloudtemple_compute_content_library.public.id
-#   name               = "20211115132417_master_linux-centos-8"
-# }
+data "cloudtemple_compute_content_library_item" "centos" {
+  content_library_id = data.cloudtemple_compute_content_library.public.id
+  name               = "20211115132417_master_linux-centos-8"
+}
 
-# data "cloudtemple_compute_datastore" "ds" {
-#   name = "ds001-bob-svc1-data4-eqx6"
-# }
+data "cloudtemple_compute_datastore" "ds" {
+  name = "ds001-bob-svc1-data4-eqx6"
+}
 
-# resource "cloudtemple_compute_virtual_machine" "content-library" {
-#   name = "from-content-library-item"
+resource "cloudtemple_compute_virtual_machine" "content-library" {
+  name = "from-content-library-item"
 
-#   content_library_id      = data.cloudtemple_compute_content_library.public.id
-#   content_library_item_id = data.cloudtemple_compute_content_library_item.centos.id
+  content_library_id      = data.cloudtemple_compute_content_library.public.id
+  content_library_item_id = data.cloudtemple_compute_content_library_item.centos.id
 
-#   datacenter_id        = data.cloudtemple_compute_virtual_datacenter.dc.id
-#   host_cluster_id      = data.cloudtemple_compute_host_cluster.flo.id
-#   datastore_cluster_id = data.cloudtemple_compute_datastore_cluster.koukou.id
-#   datastore_id         = data.cloudtemple_compute_datastore.ds.id
+  datacenter_id        = data.cloudtemple_compute_virtual_datacenter.dc.id
+  host_cluster_id      = data.cloudtemple_compute_host_cluster.flo.id
+  datastore_cluster_id = data.cloudtemple_compute_datastore_cluster.koukou.id
+  datastore_id         = data.cloudtemple_compute_datastore.ds.id
 
-#   deploy_options = {
-#     trak_sshpublickey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKpZ5juF5a/CXV9nQ0PANptTG9Gh3J0aj6yVjkF0fSkC remi@cloud-temple.com"
-#   }
+  deploy_options = {
+    trak_sshpublickey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKpZ5juF5a/CXV9nQ0PANptTG9Gh3J0aj6yVjkF0fSkC remi@cloud-temple.com"
+  }
 
-#   tags = {
-#     created_by = "Terraform"
-#   }
-# }
+  tags = {
+    created_by = "Terraform"
+  }
+}
