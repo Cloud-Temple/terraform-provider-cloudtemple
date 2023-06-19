221bf6c0631a3c8b82a6d5deb7a4c9bd9d391451
Paul BESRET <paul.besret@cloud-temple.com>
2023-06-19 11:13:14 +0200

    Fixed mistyped property on newly added 'backup_virtual_machine' controller causes virtual machines struggling to power on.

--- internal/client/backup_virtual_machine.go
+++ internal/client/backup_virtual_machine.go
@@ -14,7 +14,7 @@ func (c *BackupClient) VirtualMachine() *BackupVirtualMachineClient {
 
 type BackupVirtualMachineVolume struct {
 	Name         string `terraform:"name"`
-	Key          int    `terraform:"key"`
+	Key          string `terraform:"key"`
 	Size         int    `terraform:"size"`
 	ConfigVolume bool   `terraform:"config_volume"`
 }
@@ -24,7 +24,7 @@ type BackupVirtualMachine struct {
 	Name              string                       `terraform:"name"`
 	Moref             string                       `terraform:"moref"`
 	InternalId        string                       `terraform:"internal_id"`
-	InternalVCenterId string                       `terraform:"internal_vcenter_id"`
+	InternalVCenterId int                          `terraform:"internal_vcenter_id"`
 	VCenterId         string                       `terraform:"vcenter_id"`
 	Href              string                       `terraform:"href"`
 	MetadataPath      string                       `terraform:"matadata_path"`
