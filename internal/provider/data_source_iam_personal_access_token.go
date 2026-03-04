3568819111623e1559a525bbe6be73c743b8a31a
Paul BESRET <paul.besret@cloud-temple.com>
2026-03-04 15:15:13 +0100

    feat(#226): Removed dead code

--- internal/provider/data_source_iam_personal_access_token.go
+++ internal/provider/data_source_iam_personal_access_token.go
@@ -99,47 +99,6 @@ func dataSourcePersonalAccessTokenRead(ctx context.Context, d *schema.ResourceDa
 		return diag.FromErr(fmt.Errorf("failed to find personal access token with name %q", name))
 	}
 
-	// // Recherche par ID
-	// id := d.Get("id").(string)
-	// if id != "" {
-	// 	token, err = c.IAM().PAT().Read(ctx, id)
-	// 	if err != nil {
-	// 		return diag.FromErr(err)
-	// 	}
-	// 	if token == nil {
-	// 		return diag.FromErr(fmt.Errorf("failed to find personal access token with id %q", id))
-	// 	}
-	// } else {
-	// 	// Obtenir les IDs utilisateur et tenant
-	// 	userId, err := getUserID(ctx, c, d)
-	// 	if err != nil {
-	// 		return diag.FromErr(err)
-	// 	}
-	// 	tenantId, err := getTenantID(ctx, c, d)
-	// 	if err != nil {
-	// 		return diag.FromErr(err)
-	// 	}
-
-	// 	// Recherche par nom
-	// 	name := d.Get("name").(string)
-	// 	if name != "" {
-	// 		tokens, err := c.IAM().PAT().List(ctx, userId, tenantId)
-	// 		if err != nil {
-	// 			return diag.FromErr(fmt.Errorf("failed to list personal access tokens: %s", err))
-	// 		}
-	// 		for _, t := range tokens {
-	// 			if t.Name == name {
-	// 				token = t
-	// 				break
-	// 			}
-	// 		}
-	// 		if token == nil {
-	// 			return diag.FromErr(fmt.Errorf("failed to find personal access token with name %q", name))
-	// 		}
-	// 	} else {
-	// 		return diag.FromErr(fmt.Errorf("either id or name must be specified"))
-	// 	}
-
 	// Définir l'ID de la datasource
 	d.SetId(token.ID)
 
