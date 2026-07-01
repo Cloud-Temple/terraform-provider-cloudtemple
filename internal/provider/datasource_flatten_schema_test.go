ad25fe23f6c677bb3a50a630865fcc5ba1a7dbb8
Paul BESRET <paul.besret@cloud-temple.com>
2026-07-01 12:10:25 +0200

    style: gofmt map alignment for public_cloud_vm datasource entries

--- internal/provider/datasource_flatten_schema_test.go
+++ internal/provider/datasource_flatten_schema_test.go
@@ -360,12 +360,12 @@ var datasourceCoverage = map[string]dsCoverage{
 	"cloudtemple_public_cloud_vm_regions":            {"regions", flat(helpers.FlattenPublicCloudVMRegion)},
 	"cloudtemple_public_cloud_vm_availability_zone":  {"", flat(helpers.FlattenPublicCloudVMAvailabilityZone)},
 	"cloudtemple_public_cloud_vm_availability_zones": {"availability_zones", flat(helpers.FlattenPublicCloudVMAvailabilityZone)},
-	"cloudtemple_public_cloud_vm_flavor":            {"", flat(helpers.FlattenPublicCloudVMFlavor)},
-	"cloudtemple_public_cloud_vm_flavors":           {"flavors", flat(helpers.FlattenPublicCloudVMFlavor)},
-	"cloudtemple_public_cloud_vm_instance_family":   {"", flat(helpers.FlattenPublicCloudVMInstanceFamily)},
+	"cloudtemple_public_cloud_vm_flavor":             {"", flat(helpers.FlattenPublicCloudVMFlavor)},
+	"cloudtemple_public_cloud_vm_flavors":            {"flavors", flat(helpers.FlattenPublicCloudVMFlavor)},
+	"cloudtemple_public_cloud_vm_instance_family":    {"", flat(helpers.FlattenPublicCloudVMInstanceFamily)},
 	"cloudtemple_public_cloud_vm_instance_families":  {"instance_families", flat(helpers.FlattenPublicCloudVMInstanceFamily)},
-	"cloudtemple_public_cloud_vm_storage_type":      {"", flat(helpers.FlattenPublicCloudVMStorageType)},
-	"cloudtemple_public_cloud_vm_storage_types":     {"storage_types", flat(helpers.FlattenPublicCloudVMStorageType)},
+	"cloudtemple_public_cloud_vm_storage_type":       {"", flat(helpers.FlattenPublicCloudVMStorageType)},
+	"cloudtemple_public_cloud_vm_storage_types":      {"storage_types", flat(helpers.FlattenPublicCloudVMStorageType)},
 }
 
 // datasourceKnownGaps lists datasources deliberately NOT covered by the walker
